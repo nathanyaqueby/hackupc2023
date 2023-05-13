fb95df8ccd9012a11ecec6d0f68cf5930f3737d2
queby <nathanyaqueby21@gmail.com>
2023-05-14 01:51:31 +0200

    try streamlit-shap again

--- pages/3_💰_Predict_the_price.py
+++ pages/3_💰_Predict_the_price.py
@@ -71,45 +71,40 @@ if submit:
         st.image("images/shap1.png", use_column_width=True)
         st.image("images/shap2.png", use_column_width=True)
 
-# X = data[['square_meters','bedrooms','bathrooms']]
-# y = data['price']
+X = data[['square_meters','bedrooms','bathrooms']]
+y = data['price']
 
-# # turn x and y into numpy arrays with int values
-# X = X.to_numpy().astype(int)
-# y = y.to_numpy().astype(int)
+# turn x and y into numpy arrays with int values
+X = X.to_numpy().astype(int)
+y = y.to_numpy().astype(int)
 
-# @st.experimental_memo
-# def load_model(X, y):
-#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=7)
-#     d_train = xgboost.DMatrix(X_train, label=y_train, enable_categorical=True)
-#     d_test = xgboost.DMatrix(X_test, label=y_test, enable_categorical=True)
-#     params = {
-#         "eta": 0.01,
-#         "objective": "reg:squarederror",
-#         "subsample": 0.5,
-#         "base_score": np.mean(y_train),
-#         "eval_metric": "rmse",
-#         "n_jobs": -1,
-#     }
-#     model = xgboost.train(params, d_train, 10, evals = [(d_test, "test")], verbose_eval=100, early_stopping_rounds=20)
-#     return model
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=7)
 
-# st.title("SHAP in Streamlit")
+@st.experimental_memo
+def load_model(X, y):
 
-# # train XGBoost model
-# X_display,y_display = shap.datasets.adult(display=True)
+    # train XGBoost model
+    model = xgboost.XGBRegressor(objective='reg:squarederror', n_estimators=1000, max_depth=10, learning_rate=0.1)
+    model.fit(X_train, y_train)
 
-# model = load_model(X, y)
+    return model
 
-# # compute SHAP values
-# explainer = shap.Explainer(model, X)
-# shap_values = explainer(X)
+st.title("SHAP in Streamlit")
 
-# st_shap(shap.plots.waterfall(shap_values[0]), height=300)
-# st_shap(shap.plots.beeswarm(shap_values), height=300)
+# train XGBoost model
+X_display,y_display = shap.datasets.adult(display=True)
 
-# explainer = shap.TreeExplainer(model)
-# shap_values = explainer.shap_values(X)
+model = load_model(X, y)
 
-# st_shap(shap.force_plot(explainer.expected_value, shap_values[0,:], X_display.iloc[0,:]), height=200, width=1000)
-# st_shap(shap.force_plot(explainer.expected_value, shap_values[:1000,:], X_display.iloc[:1000,:]), height=400, width=1000)
+# compute SHAP values
+explainer = shap.Explainer(model, X)
+shap_values = explainer(X)
+
+st_shap(shap.plots.waterfall(shap_values[0]), height=300)
+st_shap(shap.plots.beeswarm(shap_values), height=300)
+
+explainer = shap.TreeExplainer(model)
+shap_values = explainer.shap_values(X)
+
+st_shap(shap.force_plot(explainer.expected_value, shap_values[0,:], X_display.iloc[0,:]), height=200, width=1000)
+st_shap(shap.force_plot(explainer.expected_value, shap_values[:1000,:], X_display.iloc[:1000,:]), height=400, width=1000)
