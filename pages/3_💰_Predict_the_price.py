e008121ba64c484c5b0bd40c2f2a43101f347bb6
queby <nathanyaqueby21@gmail.com>
2023-05-13 23:53:57 +0200

    try turning into np arrays

--- pages/3_💰_Predict_the_price.py
+++ pages/3_💰_Predict_the_price.py
@@ -68,6 +68,10 @@ avg_prices_city = data.groupby('city')['price'].mean()
 X = data[['square_meters','bedrooms','bathrooms']]
 y = data['price']
 
+# turn x and y into numpy arrays with int values
+X = X.to_numpy().astype(int)
+y = y.to_numpy().astype(int)
+
 @st.experimental_memo
 def load_model(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=7)
