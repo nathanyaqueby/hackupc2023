a2426c059bd6575f272a32b1871e1ec22102e7ee
Nathanya Queby S <nathanyaqueby21@gmail.com>
2023-05-13 03:44:40 +0200

    Create About.py

---
+++ About.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from auth0_component import login_button
+
+st.set_page_config(page_title="MLheads",
+                   page_icon="🤯",
+                   layout="wide",
+                   initial_sidebar_state="expanded")
+
+clientId = "OI8ZTGxVXXrNFPC1VS0ppNvafKAL7llW"
+domain = "mlheads.eu.auth0.com"
+
+user_info = login_button(clientId, domain = domain)
+st.write(user_info)
