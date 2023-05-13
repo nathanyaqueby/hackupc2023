7e4606330dd1326fa13395b27a527e2b4eb0a971
queby <nathanyaqueby21@gmail.com>
2023-05-13 19:09:19 +0200

    change to average

--- pages/3_💰_Predict_the_price.py
+++ pages/3_💰_Predict_the_price.py
@@ -13,25 +13,10 @@ from geopy.extra.rate_limiter import RateLimiter
 # load data
 data = pd.read_json("preprocessed_10k.json").T
 
-# get a list of all the cities
-cities = data["city"].unique().tolist()
+# get the average prices per region
+avg_prices = data.groupby("region").mean()["price"]
 
-# get a list of all the provinces
-# neighborhoods = data["neigborhood"].unique().tolist()
+st.dataframe(avg_prices)
 
-street = st.text_input("Street", "C/ de Balmes 75")
-# set up a dropdown menu to select the state between Hamburg, Berlin and Bremen
-# province = st.selectbox("Select a state", ["Hamburg", "Berlin", "Bremen"], index=0)
-province = "Barcelona"
-country = "Spain"
-
-city = st.selectbox("Select a state", cities, index=0)
-
-geolocator = Nominatim(user_agent="GTA Lookup")
-geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
-location = geolocator.geocode(street+", "+city+", "+province+", "+country)
-
-lat = location.latitude
-lon = location.longitude
-
-st.write("Latitude = {}, Longitude = {}".format(lat, lon))
+# plot a heatmap of the average prices per region
+st.map(avg_prices)
