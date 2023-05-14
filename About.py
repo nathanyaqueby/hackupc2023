0e371513f23af314c53f916b4684abd7928c7efa
queby <nathanyaqueby21@gmail.com>
2023-05-14 04:53:03 +0200

    remove css line

--- About.py
+++ About.py
@@ -13,8 +13,8 @@ st.title("MLheads 🏠")
 
 st.sidebar.info("Explore the real estate market in Spain! Meanwhile, walk through the city of Barcelona with the OpenStreetMap.")
 
-with open('style.css') as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+# with open('style.css') as f:
+#         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 st.markdown("""
     <style>
